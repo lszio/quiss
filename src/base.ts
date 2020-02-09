add6172373bd52ec96ca7713b25d58a5dbd4fe6b
liszt21 <1832666492@qq.com>
2020-02-09 18:25:54 +0800

    add badic role

--- src/base.ts
+++ src/base.ts
@@ -4,14 +4,14 @@ export default function() {
     if(!Game.creeps['Harvester']) {
         Game.spawns['Spawn1'].spawnCreep([MOVE,WORK,CARRY],'Harvester')
     }else{
-        var harvester = Game.creeps['Harvester']
+        let harvester = Game.creeps['Harvester']
         if(harvester.store.getFreeCapacity() > 0) {
             if(harvester.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 harvester.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {
-            var targets = harvester.room.find(FIND_STRUCTURES, {
+            let targets = harvester.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
                             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
@@ -28,7 +28,7 @@ export default function() {
     if(!Game.creeps['Upgrader']){
         Game.spawns['Spawn1'].spawnCreep([MOVE,WORK,CARRY],'Upgrader')
     }else{
-        var upgrader = Game.creeps['Upgrader'];
+        let upgrader = Game.creeps['Upgrader'];
         if(upgrading && upgrader.store[RESOURCE_ENERGY] == 0) {
             upgrading = false;
             upgrader.say('🔄 harvest');
@@ -53,23 +53,55 @@ export default function() {
     if(!Game.creeps['Builder']) {
         Game.spawns['Spawn1'].spawnCreep([MOVE,WORK,CARRY],'Builder')
     }else{
-        var builder = Game.creeps['Builder']
-        if(builder.store.getFreeCapacity() > 0) {
+        let builder = Game.creeps['Builder'];
+        if(building && builder.store[RESOURCE_ENERGY] == 0) {
+            building = false;
+            builder.say('🔄 harvest');
+        }
+        if(!building && builder.store.getFreeCapacity() == 0) {
+            building = true;
+            builder.say('⚡ upgrade');
+        }
+
+        if(building) {
+            let sites = builder.room.find(FIND_CONSTRUCTION_SITES);
+            if(builder.build(sites[0]) == ERR_NOT_IN_RANGE) {
+                builder.moveTo(sites[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        }
+        else {
             if(builder.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 builder.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
-        else {
-            var targets = builder.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-            });
-            if(targets.length > 0) {
-                if(builder.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    builder.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+    }
+    let repairing = false;
+    if(!Game.creeps['Repairer']) {
+        Game.spawns['Spawn1'].spawnCreep([MOVE,WORK,CARRY],'Repairer')
+    }else{
+        let repairer = Game.creeps['Repairer'];
+        if(repairing && repairer.store[RESOURCE_ENERGY] == 0) {
+            repairing = false;
+            repairer.say('🔄 harvest');
+        }
+        if(!repairing && repairer.store.getFreeCapacity() == 0) {
+            repairing = true;
+            repairer.say('⚡ upgrade');
+        }
+
+        if(repairing) {
+            let targets = repairer.room.find(FIND_STRUCTURES, {
+                filter : (structure) => {
+                    return (structure.hitsMax - structure.hits) / structure.hitsMax < 0.7
                 }
+            });
+            if(repairer.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                repairer.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        }
+        else {
+            if(repairer.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                repairer.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
     }
