36bd2a7a4c846cb723068dbf1d85ca6a21feb765
Liszt21 <1832666492@qq.com>
2020-09-29 10:31:52 +0800

    update configs

--- rollup.config.js
+++ rollup.config.js
@@ -5,12 +5,13 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import screeps from "rollup-plugin-screeps";
+import config from "./screeps.config.js";
 
 let cfg;
 const dest = process.env.DEST;
 if (!dest) {
   console.log("No destination specified - code will be compiled but not uploaded");
-} else if ((cfg = require("./screeps.json")[dest]) == null) {
+} else if ((cfg = config[dest]) == null) {
   throw new Error("Invalid upload destination");
 }
 
---
+++ screeps.config.js
@@ -0,0 +1,29 @@
+import config from "./screeps-multimeter.json";
+
+export default {
+  main: {
+    token: config.token,
+    protocol: "https",
+    hostname: "screeps.com",
+    port: 443,
+    path: "/",
+    branch: "main"
+  },
+  sim: {
+    token: config.token,
+    protocol: "https",
+    hostname: "screeps.com",
+    port: 443,
+    path: "/",
+    branch: "sim"
+  },
+  pserver: {
+    email: "username",
+    password: "Password",
+    protocol: "http",
+    hostname: "1.2.3.4",
+    port: 21025,
+    path: "/",
+    branch: "main"
+  }
+};
