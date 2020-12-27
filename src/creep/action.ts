c6e7700a38ce916971bb493b3b0c687c5a82bcec
Liszt21 <1832666492@qq.com>
2020-12-28 00:05:52 +0800

    add creep module

---
+++ src/creep/action.ts
@@ -0,0 +1,18 @@
+enum Actions {
+  Harvest,
+  Charge,
+  Mine,
+  Upgrade,
+  Build,
+  Repair,
+  Attack,
+  Transfer
+}
+
+const harvest = () => {
+  return;
+};
+
+const action = {};
+
+export default action;
